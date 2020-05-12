819a4aaf687bcde5f533422593025aa24da195fd
francoishubert <francois725@hotmail.fr>
2020-05-12 12:18:41 +0200

    update social links

--- my-app/src/products/components/Footer.tsx
+++ my-app/src/products/components/Footer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import '../Footer.css'
 
 function Footer() {
@@ -13,11 +13,11 @@ function Footer() {
                 </div>
                 <div className="footer__info">
                     <p className="u-no-margin">Conditions générales de vente.</p>
-                    <p>Contact : <span className="u-underline-grey">suppor@mgpmasks.com</span></p>
+                    <p>Contact : <a href="mailto:hello@mgpmasks.com" className="u-underline-grey u-font-grey">hello@mgpmasks.com</a></p>
                     <ul className="info__social">
-                        <li><a href="/" className="u-no-margin u-font-grey">Facebook.</a></li>
-                        <li><a href="/" className="u-no-margin u-font-grey">Instagram.</a></li>
-                        <li><a href="/" className="u-no-margin u-font-grey">Twitter.</a></li>
+                        <li><a href="https://www.facebook.com/mgp.masks/" className="u-no-margin u-font-grey">Facebook.</a></li>
+                        <li><a href="https://www.instagram.com/mgp.masks/" className="u-no-margin u-font-grey">Instagram.</a></li>
+                        <li><a href="https://twitter.com/mgpmasks" className="u-no-margin u-font-grey">Twitter.</a></li>
                     </ul>
                 </div>
             </div>
